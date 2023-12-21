ded7acff40c30d43c7c7703a96c03845456f6ec1
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-12-21 18:43:11 +0900

    feat: 使用するテンプレートを選択可能に

--- src/add_slide.py
+++ src/add_slide.py
@@ -11,7 +11,7 @@ import sys
 def find_dir_with_numberprefix(
     name_without_numberprefix, root_dir, prefix_separator="_"
 ):
-    ab_root_dir = os.path.abspath(os.path.expanduser(root_dir))
+    ab_root_dir = expand_path(root_dir)
     max_no = 0
     match_dir = None
     for d in glob.glob(os.path.join(ab_root_dir, f"*{prefix_separator}*/")):
@@ -34,18 +34,18 @@ def find_dir_with_numberprefix(
         )
 
 
-DEFAULT_ROOT_DIR = "~/slide"
-DEFAULT_TEMPLATE_DIR = "~/opt/add_slide/template/default/"
+def get_template_path(template_root, template_name):
+    return expand_path(os.path.join(template_root, template_name))
 
 
 def main():
     args = parse_args()
-    dpath = find_dir_with_numberprefix(args.project_name, args.root_dir)
+    dpath = find_dir_with_numberprefix(args.project_name, args.slide_dir)
 
     if not os.path.isdir(dpath):
         print(f"mkdir {dpath} ...")
         os.makedirs(dpath)
-        tpath = os.path.abspath(os.path.expanduser(args.template_dir))
+        tpath = get_template_path(args.template_dir, args.template_name)
         print(f"template dir({tpath})...")
         if os.path.isdir(tpath):
             for i in glob.glob(os.path.join(tpath, "*")):
@@ -57,44 +57,63 @@ def main():
                 else:
                     print(f"copyfile {i} {dst_path} ...")
                     shutil.copyfile(i, dst_path)
+        else:
+            print(f"template dir({tpath})は存在しません!!")
 
     if args.with_code:
         print(f"code {dpath} ...")
         os.system(f"code '{dpath}'")
 
 
+def expand_path(path):
+    return os.path.abspath(os.path.expanduser(path))
+
+
 def parse_args():
-    ap = argparse.ArgumentParser(
+    DEFAULT_ROOT_DIR = "~/slide"
+    DEFAULT_TEMPLATE_DIR = "~/opt/add_slide/template"
+    DEFAULT_TEMPLATE_NAME = "default"
+    parser = argparse.ArgumentParser(
         description="動画作成用のスライドプロジェクトを作成しvscodeで開く。"
         "もし既に存在するプロジェクトの場合はそのプロジェクトをvscodeで開く。"
     )
-    ap.add_argument("project_name", metavar="PROJECT_NAME", type=str, help="プロジェクト名")
-    ap.add_argument(
-        "--root-dir",
+    parser.add_argument(
+        "project_name", metavar="PROJECT_NAME", type=str, help="プロジェクト名"
+    )
+    parser.add_argument(
+        "-s",
+        "--slide-dir",
         type=str,
         default=DEFAULT_ROOT_DIR,
         help=f"スライドプロジェクトのルートディレクトリ。(デフォルト値: {DEFAULT_ROOT_DIR})",
     )
-    ap.add_argument(
+    parser.add_argument(
         "--with-code",
         dest="with_code",
         default=True,
         action="store_true",
         help="VScodeでプロジェクトを開く。",
     )
-    ap.add_argument(
+    parser.add_argument(
         "--without-code",
         dest="with_code",
         action="store_false",
         help="VScodeでプロジェクトを開かない。",
     )
-    ap.add_argument(
+    parser.add_argument(
         "--template-dir",
         default=DEFAULT_TEMPLATE_DIR,
         help=f"プロジェクトを新規作成時に雛形とするディレクトリ。(デフォルト値: {DEFAULT_TEMPLATE_DIR})",
     )
 
-    args = ap.parse_args()
+    parser.add_argument(
+        "-t",
+        "--template-name",
+        default=DEFAULT_TEMPLATE_NAME,
+        help=f"テンプレート名。(デフォルト値: {DEFAULT_TEMPLATE_NAME})",
+    )
+
+    args = parser.parse_args()
     return args
 
 
